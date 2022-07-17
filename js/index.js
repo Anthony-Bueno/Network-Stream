d281a90fcd387174ed5a918ddd4d707887b8134f
Anthony-Bueno <90065903+Anthony-Bueno@users.noreply.github.com>
2022-07-17 12:12:55 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var btnter=document.getElementById("btnter");
+var btnac=document.getElementById("btnacc");
+
+var c_terror=document.getElementById("c_terror");
+var c_accion=document.getElementById("c_accion");
+
+
+//btnter.addEventListener('click',function(){
+//c_terror.classList.remove("active");
+//c_terror.classList.add("active");
+//});
+
+//btnac.addEventListener('click',function(){
+   // c_accion.classList.remove("active");
+   // c_accion.classList.add("active");
+   // });
+
+
